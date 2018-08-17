b89f47a80989cc6c161c1dec6bc6521749ded798
Luiz Galiza <lfdgaliza@gmail.com>
2018-08-17 17:43:25 -0300

    checkpoint

---
+++ exercicios_react/ex/ChangingText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class ChangingText {
+    constructor(props) {
+        super(props)
+
+        this.state = { value: props.initialValue }
+    }
+
+    handleChange() {
+
+    }
+}

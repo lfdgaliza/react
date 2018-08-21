7c4243bc64c738d2e1ba79bd56bc915fd62afa85
Luiz Galiza <lfdgaliza@gmail.com>
2018-08-21 17:17:17 -0300

    todo-app: routing

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -2,14 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/Todo'
-import About from '../about/About'
 import Menu from '../template/Menu'
+import Routes from './Routes'
 
 export default props => (
     <div className="container">
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )

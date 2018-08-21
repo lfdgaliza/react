2d75df1968db2dc7312dbf0c01a8786473cbbb8a
Luiz Galiza <lfdgaliza@gmail.com>
2018-08-21 17:31:20 -0300

    Including todo form and list

--- todo-app/frontend/src/todo/Todo.jsx
+++ todo-app/frontend/src/todo/Todo.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/PageHeader'
+import TodoForm from '../todo/TodoForm'
+import TodoList from './TodoList';
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm></TodoForm>
+                <TodoList></TodoList>
             </div>
-        )
+        ) 
     }
 }

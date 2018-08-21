be239335660fbea6691b721d635532f4f85957bf
Luiz Galiza <lfdgaliza@gmail.com>
2018-08-21 17:26:22 -0300

    todo-app: page header e about

--- todo-app/frontend/src/about/About.jsx
+++ todo-app/frontend/src/about/About.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import PageHeader from '../template/PageHeader';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa história</h2>
+        <p>Dolor duis laborum ea id ex laboris commodo. Ut nulla proident minim sint id enim magna et incididunt ea id amet. Reprehenderit est tempor adipisicing laborum occaecat reprehenderit dolor ex cupidatat duis enim sint laborum cupidatat. Consequat deserunt culpa amet esse quis cupidatat consequat cupidatat exercitation. Magna esse non exercitation do et. Aute nostrud non et cupidatat laborum duis aute aute aute.</p>
+
+        <h2>Missão e visão</h2>
+        <p>Laborum esse quis excepteur aute Lorem deserunt laboris elit et.</p>
+
+        <h2>Imprensa</h2>
+        <p>Quis ea occaecat ullamco elit culpa dolor irure.</p>
     </div>
 )
